44934677efd6481d67f1b58840db59d9743efe53
1Ambulant <abgroup.ulisse@gmail.com>
2026-07-13 18:44:57 +0000

    Add FlashMecano React source (App.jsx, main.jsx, index.css)

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FlashMecanoApp from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FlashMecanoApp />
+  </React.StrictMode>
+);
